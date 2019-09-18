69dd75e5aa7701cebada578410c17aaf2add1e05
Kellen Frodelius-Fujimoto <kellen@kellenfujimoto.com>
2019-09-18 13:45:26 +0200

    Add JSDoc comment to MaskedInput

--- src/masked-input/masked-input.tsx
+++ src/masked-input/masked-input.tsx
@@ -8,6 +8,27 @@ export interface MaskedInputProps {
   onChange?: React.ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>;
 }
 
+/**
+ * @callback onValueChange
+ * @param {string} value The stripped value from the Input's `onChange` callback
+ * @return {void}
+ */
+
+/**
+ * @callback onChange
+ * @param {event} event The direct event passed back from the `Input`'s `onChange` callback
+ * @return {void}
+ */
+
+/**
+ * A basic masked input component which wraps `material-ui`'s `Input` component
+ * @param {string} value The value of the input element
+ * @param {string=} mask The text mask to be placed in front of the input's value
+ * @param {onValueChange} onValueChange Callback which fires from the child `Input`'s `onChange` callback
+ * @param {onChange} onChange Direct pass-through of the `Input`'s `onChange` callback.
+ * WARNING: reading the value from this can lead to unexpected behavior if a user backspaces on an empty value
+ * @constructor
+ */
 const MaskedInput: React.FC<MaskedInputProps> = ({ value, mask, onValueChange, onChange }) => {
   const maskedInput = (mask || '') + value;
   const maskSearch  = mask && new RegExp(`^${ mask }`);
